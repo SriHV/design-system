0999e0048e2efe2054c4c9b64f5757f5ae434376
SriHV <sri.harsha.v.parupudi@ons.gov.uk>
2025-02-20 20:27:07 +0000

    testing

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -8,7 +8,10 @@ if (window.google_tag_manager !== undefined) {
         gtag('event', type, { ...data });
         console.log('Data sent to Data Layer');
     };
-} else if (process.env.RUNNING_IN_CI !== 'true') {
+} else if (process.env.RUNNING_IN_CI == 'true') {
+    console.log("Printing");
+}
+ else if (process.env.RUNNING_IN_CI !== 'true') {
     console.log('RUNNING_IN_CI:', process.env.RUNNING_IN_CI || 'Not Set');
     console.log('Google analytics not connected');
 }
